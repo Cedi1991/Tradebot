533a43c5f1004c820b9e8944a03519eecf1186ee
Cedi1991 <cedi.m@web.de>
2024-06-14 14:26:15 +0300

    Create risiko_manager.py

---
+++ risiko_manager.py
@@ -0,0 +1,9 @@
+# risikomanagement/risiko_manager.py
+
+class RisikoManager:
+    def __init__(self):
+        pass
+
+    def signale_anpassen(self, signale):
+        # Platzhalter für Risikomanagement-Logik
+        return signale
