f0d62ea4202deafb9507d8b45e340a243ad797dd
Cedi1991 <cedi.m@web.de>
2024-06-14 14:26:41 +0300

    Create protokollierer.py

---
+++ protokollierer.py
@@ -0,0 +1,9 @@
+# protokollierung/protokollierer.py
+
+class Protokollierer:
+    def __init__(self):
+        pass
+
+    def protokollieren(self, signale):
+        # Platzhalter für Protokollierungslogik
+        print(signale)
